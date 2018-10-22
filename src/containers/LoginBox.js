ea9660d4d1d0e1121fd74367817594d8e62ef5a7
Unknown <yuchiehchen@outlook.com>
2018-10-22 17:02:07 +0800

    在 LoginBox container 指定要使用 onClickSubmit  來派分(dispatch)一個 action

--- src/containers/LoginBox.js
+++ src/containers/LoginBox.js
@@ -1,5 +1,7 @@
 import { connect } from 'react-redux';
 import LobinBox from '../components/LoginBox'
+import * as actions from '../actions/LoginBoxActions';
+
 const mapStateToProps = (state) => {
     return {
         message: state.message,
@@ -7,6 +9,11 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = (dispatch) => {
+    return {
+        onClickSubmit() {
+            dispatch(actions.onClickSubmit({message: 'clicked'}));
+        }
+    }
 };
 
 export default connect(
