fbdfbe14d5e48f0938c78e9ef45bb52ba613df1a
Unknown <yuchiehchen@outlook.com>
2018-10-22 17:09:30 +0800

    RootReducer 要監控這個 action (identityOnClickSubmit)

--- src/reducers/RootReducer.js
+++ src/reducers/RootReducer.js
@@ -1,9 +1,15 @@
+import * as actions from '../actions/LoginBoxActions';
+
 const initState = {
   message: 'init message',
 };
 const reducer = (state = initState, action) => {
-  const {type} = action;
+  const {type, payload} = action;
   switch (type) {
+      case actions.identityOnClickSubmit: {
+        const {message} = payload;
+        return Object.assign({}, {message});
+      }
       default:
           return state;
   }
