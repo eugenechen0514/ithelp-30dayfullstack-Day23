82da74d286c6e8ade847f27cbc1abe3411371a48
Unknown <yuchiehchen@outlook.com>
2018-10-22 16:10:25 +0800

    Prue component: LoginBox

---
+++ src/components/LoginBox.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LoginBox extends Component {
+  render() {
+    return (
+      <div>
+        message: {this.props.message}
+      </div >
+    );
+  }
+}
+
+export default LoginBox;
