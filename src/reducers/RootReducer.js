b5af49df72e7e3b7770bb4cde0619e3286deaeeb
Unknown <yuchiehchen@outlook.com>
2018-10-22 15:04:54 +0800

    建立 RootReducer

---
+++ src/reducers/RootReducer.js
@@ -0,0 +1,10 @@
+const initState = {};
+const reducer = (state = initState, action) => {
+  const {type} = action;
+  switch (type) {
+      default:
+          return state;
+  }
+};
+
+export default reducer;
