e0375928fc82d8cac554a9dac0d95dc540d80a46
Unknown <yuchiehchen@outlook.com>
2018-10-22 16:14:37 +0800

    Container: LoginBox

---
+++ src/containers/LoginBox.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+import LobinBox from '../components/LoginBox'
+const mapStateToProps = (state) => {
+    return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(LobinBox);
