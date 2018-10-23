641b1de2cfdc8b7f3e907572fd7aa8cb85bb81ea
Unknown <yuchiehchen@outlook.com>
2018-10-23 08:45:10 +0800

    使用 Chrome Extension: Redux DevTools

--- src/stores/configureStore.dev.js
+++ src/stores/configureStore.dev.js
@@ -1,10 +1,13 @@
-import { createStore } from 'redux';
+import { compose, createStore } from 'redux';
 import RootReducer from '../reducers/RootReducer';
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export function configureStore(initialState) {
     const store = createStore(
         RootReducer,
         initialState,
+        composeEnhancer()
     );
     return store;
 }
